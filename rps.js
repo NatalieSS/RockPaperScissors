f8aa27dc55f2580434703a28e3ce5b5eb55cade0
Natalie <natalie.s.sharpe@gmail.com>
2016-09-24 14:14:10 -0500

    started adding JS and jQuery

--- rps.js
+++ rps.js
@@ -1,4 +1,4 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
 //     //button hover visual cue
  
@@ -11,4 +11,84 @@
 // 		    $(this).removeClass('active');
 // 			});
 
-// })
+
+
+
+// when user submits time form, set the timer for that time and begin 
+// countdown, which will stop at zero
+
+// warn user with popup when 10 secs left
+
+// choices only become possible once countdown starts
+
+
+
+var userChoice;
+var botChoice;
+
+// when user chooses r, p, or s, change btn background color and set userChoice
+
+
+var userChooses = function() {
+  $('#rock').click(function() {
+    userChoice == "rock";
+  });
+  $('#paper').click(function() {
+    userChoice == "paper";
+  });
+  $('#scissors').click(function() {
+    userChoice == "scissors";
+  });
+  console.log(userChoice);
+  botChooses();
+};
+
+// also trigger computer random choice and display that choice
+	// on userChoice definition...
+var botChooses = function() {
+  var x = Math.random();
+  if (x < .33) {
+  	botChoice = "rock";
+  } else if (.33 <= x < .66) {
+  	botChoice = "paper";
+  } else {
+  	botChoice = "scissors";
+  }
+  updateScores();
+}
+
+// possibly add little alert here saying who won this round
+
+// after bot chooses, update scores
+var updateScores = function() {
+  if (userChoice == botChoice) {
+    alert ("Tie!");
+  } else if (userChoice == "rock") {
+    if (botChoice == "scissors") {
+      alert ("You win!");
+    } else {
+      alert ("You lose.");
+    };
+  } else if (userChoice == "paper") {
+    if (botChoice == "rock") {
+      alert ("You win!");
+    } else {
+      alert ("You lose.");
+    };
+  } else if (userChoice == "scissors") {
+    if (botChoice == "paper") {
+      alert ("You win!");
+    } else {
+      alert ("You lose");
+    };
+  };
+
+}
+
+// when timer reaches 0, stop countdown, disable play, and display who won
+
+// clicking restart button restarts timer and zeroes out the scores
+
+
+$('.btn-choose').on('click', userChooses());
+});
