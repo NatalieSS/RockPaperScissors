af92275e78204ce438e221f71ecc9eced1f49c87
Natalie <natalie.s.sharpe@gmail.com>
2016-09-23 21:24:48 -0500

    initial files, layout, and styling

---
+++ rps.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+
+//     //button hover visual cue
+ 
+// 	$("col a").hover(function(){
+// 		event.preventDefault();
+// 			$(this).css("color", "green");
+// 		 	$(this).addClass('active');
+// 		},function(){
+// 				$(this).css("color", "black");
+// 		    $(this).removeClass('active');
+// 			});
+
+// })
